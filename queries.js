a17f5244fdd504103ed907c8c87df0f0e3f8cc50
Tucker <borisblade17@gmail.com>
2022-11-27 13:55:54 -0600

    Add logging - Tucker

--- queries.js
+++ queries.js
@@ -7,6 +7,7 @@ const getAllNodes = (callback) => {
 }
 
 const getNodeById = (nodeId, callback) => {
+  console.log(config.db);
   pool.query('SELECT * FROM node WHERE node_id = $1', [nodeId], (error, results) => {
     callback(error, results);
   });
