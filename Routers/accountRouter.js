d5666e97fc048c650c36e99f9914d1f20906c94d
Tucker <borisblade17@gmail.com>
2022-11-27 13:24:25 -0600

    add logging - Tucker

--- Routers/accountRouter.js
+++ Routers/accountRouter.js
@@ -10,6 +10,7 @@ router.post('/register', async function (request, response) {
   const { username, password, email, name } = request.body;
 
   const savedAccount = await db.getAccountByUsername(username);
+  console.log(savedAccount);
   if (savedAccount != undefined) {
     response.status(200).json("Account with Username Already Exists.");
     return;
